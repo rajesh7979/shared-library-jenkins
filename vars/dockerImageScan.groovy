cf4185aaf38476194379c31ff9d6fb1a01fca275
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-15 23:18:19 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,8 @@
 
   def call() {
     // Load Trivy parameters and perform Trivy scan
-    def loadTrivyParameters() {
+   def loadTrivyParameters() {
+    try {
         def libraryRoot = libraryResource.rootDir.toString()
         def paramsFile = readYaml(file: "${libraryRoot}/../resources/params.yaml")
 
@@ -11,29 +12,12 @@
         def targetImage = paramsFile.TARGET_IMAGE
         def severityThreshold = paramsFile.SEVERITY_THRESHOLD
         def ignoreUnfixed = paramsFile.IGNORE_UNFIXED
-        def generateHtmlReport = paramsFile.GENERATE_HTML_REPORT
-        def reportOutputPath = paramsFile.REPORT_OUTPUT_PATH
-        def buildOnFailure = paramsFile.BUILD_ON_FAILURE
 
-        // Execute Trivy scan
-        def trivyCommand = "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${trivyImage} --file ${dockerfilePath} --severity ${severityThreshold.toLowerCase()} ${ignoreUnfixed ? '--ignore-unfixed' : ''} ${targetImage}"
+        // Use or process the parameters here
 
-        // Include report generation if enabled
-        if (generateHtmlReport) {
-            trivyCommand += " --format html --output ${reportOutputPath}"
-        }
-
-        // Execute the Trivy command
-        def trivyScanResult = sh(script: trivyCommand, returnStatus: true)
-
-        // Check for build pass or fail based on vulnerability severity
-        if (buildOnFailure && trivyScanResult != 0) {
-            error "Trivy found vulnerabilities that exceed the specified threshold. Failing the build."
-        } else {
-            echo "Trivy scan completed without critical vulnerabilities. Proceeding with the build."
-        }
+    } catch (Exception e) {
+        error "Error loading Trivy parameters: ${e.message}"
     }
+}
 
-    // Call the function to load Trivy parameters and perform the scan
-    loadTrivyParameters()
 }
