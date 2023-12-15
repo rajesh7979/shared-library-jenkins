497b800d0bb54eb3574124bb9d60100c701c507d
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-15 16:20:18 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,8 @@
 def call(String project,String ImageTag, String hubUser){
 
     sh """
-      sudo  docker image build -t ${hubUser}/${project} .
-      sudo  docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-      sudo  docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      sudo -s docker image build -t ${hubUser}/${project} .
+      sudo -s docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+      sudo -s  docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
