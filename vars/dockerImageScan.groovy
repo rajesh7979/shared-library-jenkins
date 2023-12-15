25c1c3601d7e5d4d9055db9f58fe2e2acc663a7b
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-15 23:26:12 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,22 +1,29 @@
+def call() {
+    def loadTrivyParameters = {
+        try {
+            def libraryRoot = libraryResource.rootDir.toString()
+            def paramsFile = readYaml(file: "${libraryRoot}/../resources/params.yaml")
 
-  def call() {
-    // Load Trivy parameters and perform Trivy scan
-   def loadTrivyParameters() 
-    try {
-        def libraryRoot = libraryResource.rootDir.toString()
-        def paramsFile = readYaml(file: "${libraryRoot}/../resources/params.yaml")
+            // Access Trivy parameters
+            def trivyImage = paramsFile.TRIVY_IMAGE
+            def dockerfilePath = paramsFile.DOCKERFILE_PATH
+            def targetImage = paramsFile.TARGET_IMAGE
+            def severityThreshold = paramsFile.SEVERITY_THRESHOLD
+            def ignoreUnfixed = paramsFile.IGNORE_UNFIXED
 
-        // Access Trivy parameters
-        def trivyImage = paramsFile.TRIVY_IMAGE
-        def dockerfilePath = paramsFile.DOCKERFILE_PATH
-        def targetImage = paramsFile.TARGET_IMAGE
-        def severityThreshold = paramsFile.SEVERITY_THRESHOLD
-        def ignoreUnfixed = paramsFile.IGNORE_UNFIXED
+            echo "Trivy Image: ${trivyImage}"
+            echo "Dockerfile Path: ${dockerfilePath}"
+            echo "Target Image: ${targetImage}"
+            echo "Severity Threshold: ${severityThreshold}"
+            echo "Ignore Unfixed: ${ignoreUnfixed}"
 
-        // Use or process the parameters here
-
-    } catch (Exception e) {
-        error "Error loading Trivy parameters: ${e.message}"
+            // Execute Trivy scan using the loaded parameters
+            sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${trivyImage} --file ${dockerfilePath} --severity ${severityThreshold.toLowerCase()} ${ignoreUnfixed ? '--ignore-unfixed' : ''} ${targetImage}"
+        } catch (Exception e) {
+            error "Error loading Trivy parameters: ${e.message}"
+        }
     }
 
+    // Call the closure to load Trivy parameters and perform the scan
+    loadTrivyParameters()
 }
