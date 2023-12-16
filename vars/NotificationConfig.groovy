b08e20f45a85f9057c86636204d9f3cab519640a
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-16 20:25:22 +0530

    Create NotificationConfig.groovy

---
+++ vars/NotificationConfig.groovy
@@ -0,0 +1,14 @@
+def getTeamNotification(teamName) {
+    def workspace = env.WORKSPACE
+    def configFile = "${workspace}/params.yaml"
+    def notificationConfig = [:]
+
+    try {
+        def configFileContent = readFile configFile
+        notificationConfig = readYaml text: configFileContent
+    } catch (Exception e) {
+        println("Failed to load notification configuration: ${e.message}")
+    }
+
+    return notificationConfig.teams[teamName] ?: [:]
+}
