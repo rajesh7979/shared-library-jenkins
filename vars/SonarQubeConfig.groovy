b5bddd4ce7c6656fe03f468d4436065b917cd9f9
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-16 18:26:54 +0530

    Update SonarQubeConfig.groovy

--- vars/SonarQubeConfig.groovy
+++ vars/SonarQubeConfig.groovy
@@ -1,17 +1,21 @@
+package com.acme.jenkins // Update with your package name
+
 import org.yaml.snakeyaml.Yaml
 
-def getSonarQubeConfig() {
-    def workspace = env.WORKSPACE
-    def configFile = "${workspace}/params.yaml" // YAML file path in the workspace
-    def sonarConfig = [:]
+class SonarQubeConfig {
+    def static getConfig() {
+        def workspace = pwd() // Get the workspace path
+        def configFile = "${workspace}/params.yaml" // YAML file path in the workspace
+        def sonarConfig = [:]
 
-    try {
-        def yaml = new Yaml()
-        def inputStream = new FileInputStream(new File(configFile))
-        sonarConfig = yaml.load(inputStream)
-    } catch (Exception e) {
-        println("Failed to load SonarQube configuration: ${e.message}")
+        try {
+            def yaml = new org.yaml.snakeyaml.Yaml()
+            def inputStream = new FileInputStream(new File(configFile))
+            sonarConfig = yaml.load(inputStream)
+        } catch (Exception e) {
+            println("Failed to load SonarQube configuration: ${e.message}")
+        }
+
+        return sonarConfig.sonarqube ?: [:]
     }
-
-    return sonarConfig.sonarqube ?: [:]
 }
