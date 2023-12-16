ee8befdbef6efcd233f19266b3acd292b8e3aba3
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-16 11:00:53 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,38 @@
 def call() {
     def loadTrivyParameters = {
         try {
+            def paramsFile = readFile("${env.WORKSPACE}/../resources/params.yaml")
+
+            def params = new groovy.yaml.Yaml().parseText(paramsFile)
+
+            // Access Trivy parameters
+            def trivyImage = params.TRIVY_IMAGE
+            def dockerfilePath = params.DOCKERFILE_PATH
+            def targetImage = params.TARGET_IMAGE
+            def severityThreshold = params.SEVERITY_THRESHOLD
+            def ignoreUnfixed = params.IGNORE_UNFIXED
+
+            echo "Trivy Image: ${trivyImage}"
+            echo "Dockerfile Path: ${dockerfilePath}"
+            echo "Target Image: ${targetImage}"
+            echo "Severity Threshold: ${severityThreshold}"
+            echo "Ignore Unfixed: ${ignoreUnfixed}"
+
+            // Execute Trivy scan using the loaded parameters
+            sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${trivyImage} --file ${dockerfilePath} --severity ${severityThreshold.toLowerCase()} ${ignoreUnfixed ? '--ignore-unfixed' : ''} ${targetImage}"
+        } catch (Exception e) {
+            error "Error loading Trivy parameters: ${e.message}"
+        }
+    }
+
+    // Call the closure to load Trivy parameters and perform the scan
+    loadTrivyParameters()
+}
+
+
+/*def call() {
+    def loadTrivyParameters = {
+        try {
           //  def libraryRoot = libraryResource.rootDir.toString()
             def paramsFile = readYaml(file: "../resources/params.yaml")
 
@@ -26,4 +58,4 @@ def call() {
 
     // Call the closure to load Trivy parameters and perform the scan
     loadTrivyParameters()
-}
+}*/
