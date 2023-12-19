faa439455d1531586e1490783879a3c5c9527b2b
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-19 22:25:09 +0530

    Update NotificationConfig.groovy

--- vars/NotificationConfig.groovy
+++ vars/NotificationConfig.groovy
@@ -1,17 +1,4 @@
-class NotificationConfig {
-    def getTeamNotification(teamName) {
-        def workspace = env.WORKSPACE
-        def configFile = "${workspace}/params.yaml"
-        def notificationConfig = [:]
-
-        try {
-            def configFileContent = readFile configFile
-            notificationConfig = readYaml text: configFileContent
-        } catch (Exception e) {
-            println("Failed to load notification configuration: ${e.message}")
-        }
-
-        def teamConfig = notificationConfig?.teams?.find { it.key == teamName }?.value
-        return teamConfig ?: [:]
-    }
+def sendTeamsNotification(webhookUrl, message) {
+    // Send a notification to Microsoft Teams using webhookUrl and message
+    teams([webhookUrl: webhookUrl, message: message])
 }
