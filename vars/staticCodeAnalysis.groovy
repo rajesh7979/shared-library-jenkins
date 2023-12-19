3d454f0ac2619492aa06206c2bccf93e8a973789
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-19 16:20:08 +0530

    Update staticCodeAnalysis.groovy

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,7 +1,19 @@
-def call(credentialsId){
+// SonarUtils.groovy in shared library
+
+def runSonarAnalysis(String petclinic, String petclinic) {
+    // Fetch SonarQube server details from Jenkins global configuration
+    def sonarScannerHome = tool 'SonarQubeScanner' // Name of the SonarQube Scanner tool in Jenkins
+
+    // Run SonarQube analysis
+    sh "${sonarScannerHome}/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey}"
+}
+
+
+
+/*def call(credentialsId){
     withSonarQubeEnv('sonar') {
         sh 'mvn sonar:sonar'
-    }
+    }*/
 //    withSonarQubeEnv(credentialsId: sonar-token) {
 //        sh 'mvn sonar:sonar'
 //   } 
