cda0efc3d17bb26b3fe6eaeabeab3d851cf3827d
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-19 20:50:30 +0530

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,11 +1,6 @@
 // loadConfig.groovy in your shared library under 'vars' directory
 
-def call() {
-    def loadNotificationConfigFromYaml() {
-        def yamlContent = readYaml(file: "${libraryResource.rootDir}/resources/params.yaml")
-
-        return yamlContent.notificationConfig ?: [:]
-    }
-
-    return this
+def loadNotificationConfigFromYaml() {
+    def yamlContent = readYaml file: "${libraryResource.rootDir}/resources/params.yaml"
+    return yamlContent.notificationConfig ?: [:]
 }
