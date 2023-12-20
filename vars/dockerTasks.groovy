af5e54f4ed704108632eba01c1113080136d8532
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-20 11:18:10 +0530

    Update dockerTasks.groovy

--- vars/dockerTasks.groovy
+++ vars/dockerTasks.groovy
@@ -2,7 +2,7 @@ def call(Map params) {
     return {
         // Extract parameters or provide defaults
         def dockerfilePath = params.dockerfilePath ?: 'docker/Dockerfile'
-        def imageName = params.imageName ?: 'my-image'
+        def imageName = params.imageName ?: 'cartapp'
         def imageTag = params.imageTag ?: 'latest'  // Default tag is 'latest'
         
         // Building Docker image using the provided Dockerfile path, image name, and tag
