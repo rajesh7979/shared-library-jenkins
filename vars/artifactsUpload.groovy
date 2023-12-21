f7e91006a74dcf7ba61ed02d39caa8c5fea940d4
rajesh7979 <43139169+rajesh7979@users.noreply.github.com>
2023-12-21 23:47:45 +0530

    Update artifactsUpload.groovy

--- vars/artifactsUpload.groovy
+++ vars/artifactsUpload.groovy
@@ -1,5 +1,6 @@
 def call(String jfrogArtifactory, String imageName) { 
   sh "docker login -u ${jfrog_username} fisdemo1.jfrog.io -p ${jfrog_pass}"
+  sh "docker build -t ${jfrogArtifactory}/${imageName}:latest -f docker/Dockerfile .
   sh "docker tag ${jfrogArtifactory}/${imageName} ${jfrogArtifactory}/${imageName}:latest"
   sh "docker push ${jfrogArtifactory}/${imageName}:latest"
 }
